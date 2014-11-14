bdee6aa8a2b1baaea1a1b7b198eee1a99b3223e0
Saad Masood <itssaad@icloud.com>
2014-11-14 12:18:20 +0500

    init commit

---
+++ db/migrate/20141114071030_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :middle_name
+      t.string :last_name
+      t.string :prefix
+      t.string :suffix
+      t.string :nickname
+      t.string :organization
+      t.string :job_title
+      t.string :department
+      t.date :birthdate
+      t.text :note
+      t.datetime :creation_date
+      t.datetime :modification_date
+      t.string :app_id
+
+      t.timestamps
+    end
+  end
+end
